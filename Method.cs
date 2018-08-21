34caf486d14480b7c824b2e898103f7a0e558386
Summer <1725469554@qq.com>
2018-08-21 14:48:34 +0800

    daily update
    the function Getxzdangle which named as _Getxzdangle is not correct in some scene.
    you cna still use the function Getxzdangle cause i have built it  in another way
    point out my errors please

--- Method.cs
+++ Method.cs
@@ -16,23 +16,23 @@ namespace URWPGSim2D.Strategy
         /// 实时返回角速度档位
         /// </summary>
         /// <param name="angvel">由Getxzdangle（）函数返回的角度值</param>
-        /// <returns></returns>
+        /// <returns>返回角速度档位</returns>
         public static int TransfromAngletoTCode(float angvel)
         {
             if (angvel < 0)
-                return 14;
+                return 0;
             else if (angvel == 0)
                 return 7;
             else
-                return 0;
+                return 14;
         }
         /// <summary>
         /// 返回鱼到达定点需要转的角度
         /// </summary>
         /// <param name="fish">机器鱼对象</param>
         /// <param name="aimPosition">目标点坐标</param>
-        /// <returns></returns>
-        public static float Getxzdangle(RoboFish fish, xna.Vector3 aimPosition)
+        /// <returns>鱼到达定点需要转的角度</returns>
+        public static float _Getxzdangle(RoboFish fish, xna.Vector3 aimPosition)
         {
             float cur_x = fish.PositionMm.X;
             float cur_z = fish.PositionMm.Z;
@@ -117,6 +117,35 @@ namespace URWPGSim2D.Strategy
             }
             return xzdangle;
         }
+
+        /// <summary>
+        /// 返回转动方向 
+        /// </summary>
+        /// <param name="fish"></param>
+        /// <param name="aimPosition"></param>
+        /// <returns>-1表示左转 0表示直行 1表示右转</returns>
+        public static int Getxzdangle(RoboFish fish, xna.Vector3 aimPosition)
+        {
+            xna.Vector3 aimVector;
+            aimVector.X = aimPosition.X - fish.PositionMm.X;
+            aimVector.Z = aimPosition.Z - fish.PositionMm.Z;
+            aimVector.Y = 0;
+            //float aimAngle = StrategyHelper.Helpers.GetAngleDegree(aimVector);
+            xna.Vector3 fishRad = new xna.Vector3((float)Math.Cos(fish.BodyDirectionRad), 0, (float)Math.Sin(fish.BodyDirectionRad));
+            //公式：θ=atan2(v2.y,v2.x)−atan2(v1.y,v1.x)
+            //atan2的取值范围是[−π,π]，在进行相减之后得到的夹角是在[−2π,2π]，
+            //因此当得到的结果大于π时，对结果减去2π，当结果小于−π时，对结果加上2π
+            //虽然与一般坐标方向不一致，但是象限都是3 4 1 2的顺序，所以仍然成立
+            //但是仍需验证
+            float theta = (float)Math.Atan2(aimVector.Z, aimVector.X) - (float)Math.Atan2(fishRad.Z, fishRad.X);
+            if (theta > Math.PI)
+                theta -= (float)(2 * Math.PI);
+            else if (theta < -Math.PI)
+                theta += (float)(2 * Math.PI);
+            if (theta > 0) return 1;
+            else if (theta < 0) return -1;
+            else return 0;
+        }
         #endregion
 
         #region 获得两点距离
@@ -124,7 +153,7 @@ namespace URWPGSim2D.Strategy
         {
             return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Z - b.Z, 2));
         }
-        
+
         #endregion
     }
 }
